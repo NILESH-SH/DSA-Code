7dc9ecd0af26b08a7c2d75483d859729b75fec19
NILESH-SH <Virtualid0@gmail.com>
2023-09-24 17:35:42 +0530

    Basic Array Questions

---
+++ Array/ArrayBasic.java
@@ -0,0 +1,16 @@
+/**
+ * PairSum
+ */
+public class ArrayBasic {
+    static void arraysum(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        arraysum(arr);
+    }
+}
